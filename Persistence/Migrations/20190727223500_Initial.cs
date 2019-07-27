38e56161dc82d7380d154c7bf7c23892db8ac3b7
Marlena Anton <marantondev@gmail.com>
2019-07-28 00:36:48 +0200

    Created initial migration

---
+++ Persistence/Migrations/20190727223500_Initial.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PersonalInfoSampleApp.Persistence.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "City",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CityName = table.Column<string>(maxLength: 40, nullable: false),
+                    Powiat = table.Column<string>(maxLength: 40, nullable: false),
+                    Voivodeship = table.Column<string>(maxLength: 40, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Street = table.Column<string>(maxLength: 50, nullable: false),
+                    ResidenceNumber = table.Column<string>(maxLength: 20, nullable: false),
+                    PostalNumber = table.Column<string>(maxLength: 6, nullable: false),
+                    CityId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Address_City_CityId",
+                        column: x => x.CityId,
+                        principalTable: "City",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonalInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    UseSameAddress = table.Column<bool>(nullable: false),
+                    ResidenceAddressId = table.Column<int>(nullable: false),
+                    CorrespondenceAddressId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonalInfo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PersonalInfo_Address_CorrespondenceAddressId",
+                        column: x => x.CorrespondenceAddressId,
+                        principalTable: "Address",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PersonalInfo_Address_ResidenceAddressId",
+                        column: x => x.ResidenceAddressId,
+                        principalTable: "Address",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_CityId",
+                table: "Address",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonalInfo_CorrespondenceAddressId",
+                table: "PersonalInfo",
+                column: "CorrespondenceAddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonalInfo_ResidenceAddressId",
+                table: "PersonalInfo",
+                column: "ResidenceAddressId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PersonalInfo");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropTable(
+                name: "City");
+        }
+    }
+}
