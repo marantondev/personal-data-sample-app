e1877d734564cdee3d26d9d8b4394df1e267220f
Marlena Anton <marantondev@gmail.com>
2019-07-28 00:20:07 +0200

    Created persistence project

---
+++ Persistence/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalInfoSampleApp.Model;
+
+namespace PersonalInfoSampleApp.Persistence
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<PersonalInfo> PersonalInfo { get; set; }
+        public DbSet<City> City { get; set; }
+    }
+}
