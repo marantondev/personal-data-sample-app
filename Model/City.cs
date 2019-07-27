b0670c4e764ce1883081e07102f47af957887ebd
Marlena Anton <marantondev@gmail.com>
2019-07-27 23:52:43 +0200

    Created model classes

---
+++ Model/City.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalInfoSampleApp.Model
+{
+    public class City
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string CityName { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string Powiat { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string Voivodeship { get; set; }
+    }
+}
