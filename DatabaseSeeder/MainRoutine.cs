b776368ab07d36a54876ec74691193c9727c6a92
Marlena Anton <marantondev@gmail.com>
2019-07-28 10:40:22 +0200

    Implemented database seeder

---
+++ DatabaseSeeder/MainRoutine.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using PersonalInfoSampleApp.Model;
+using PersonalInfoSampleApp.Persistence;
+
+namespace PersonalInfoSampleApp.DatabaseSeeder
+{
+    internal sealed class MainRoutine
+    {
+        private readonly string _databasePath;
+
+        public MainRoutine()
+        {
+            _databasePath = "Server=(localdb)\\mssqllocaldb;Database=PersonalInfoSampleAppContext-8f90eb8e-6278-45c9-8a4b-810a6ea73ddd;Trusted_Connection=True;MultipleActiveResultSets=true";
+        }
+
+        public MainRoutine(string databasePath)
+        {
+            _databasePath = databasePath;
+        }
+
+        internal void Run()
+        {
+            using(var dbContext = CreateDatabaseContext())
+            {
+                dbContext.Database.Migrate();
+                var entries = DeserializeEntries();
+                dbContext.AttachRange(entries);
+                dbContext.SaveChanges();
+            }
+        }
+
+        private City[] DeserializeEntries()
+        {
+            var serializedCities = Properties.Resources.Cities;
+            return JsonConvert.DeserializeObject<City[]>(serializedCities);
+        }
+
+        private DatabaseContext CreateDatabaseContext()
+        {
+            var builder = new DbContextOptionsBuilder<DatabaseContext>();
+            builder.UseSqlServer(_databasePath);
+            return new DatabaseContext(builder.Options);
+        }
+    }
+}
---
+++ DatabaseSeeder/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PersonalInfoSampleApp.DatabaseSeeder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                new MainRoutine(args[0]).Run();
+            }
+            new MainRoutine().Run();
+        }
+    }
+}
