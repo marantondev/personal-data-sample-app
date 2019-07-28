e87352b5d9614bbe7efeb711aa43030c8f1fe49e
Marlena Anton <marantondev@gmail.com>
2019-07-28 16:06:28 +0200

    Added translation strings to viewmodels

--- PersonalInfoSampleApp/Pages/Form/ViewModel/AddressViewModel.cs
+++ PersonalInfoSampleApp/Pages/Form/ViewModel/AddressViewModel.cs
@@ -4,23 +4,23 @@ namespace PersonalInfoSampleApp.Pages.Form.ViewModel
 {
     public class AddressViewModel
     {
-        [StringLength(50)]
-        [Required]
+        [StringLength(50, ErrorMessage = "LengthViolation")]
+        [Required(ErrorMessage = "FieldRequired")]
         [Display(Name = "Street")]
         public string Street { get; set; }
 
-        [StringLength(20)]
-        [Required]
+        [StringLength(20, ErrorMessage = "LengthViolation")]
+        [Required(ErrorMessage = "FieldRequired")]
         [Display(Name = "ResidenceNumber")]
         public string ResidenceNumber { get; set; }
 
-        [StringLength(6)]
-        [RegularExpression(@"^\d{2}-\d{3}$")]
-        [Required]
+        [StringLength(6, ErrorMessage = "LengthViolation")]
+        [RegularExpression(@"^\d{2}-\d{3}$", ErrorMessage ="PostalCodeFormat")]
+        [Required(ErrorMessage = "FieldRequired")]
         [Display(Name = "PostalNumber")]
         public string PostalNumber { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "FieldRequired")]
         [Display(Name = "City")]
         public int CityId { get; set; }
     }
