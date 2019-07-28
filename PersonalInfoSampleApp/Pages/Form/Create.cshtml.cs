93454c0fc29e342ccf23198be09af9c1380dc804
Marlena Anton <marantondev@gmail.com>
2019-07-28 02:26:56 +0200

    Scaffolded form

---
+++ PersonalInfoSampleApp/Pages/Form/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PersonalInfoSampleApp.Model;
+using PersonalInfoSampleApp.Persistence;
+
+namespace PersonalInfoSampleApp.Pages.Form
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DatabaseContext _context;
+
+        public CreateModel(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public PersonalInfo PersonalInfo { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.PersonalInfo.Add(PersonalInfo);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ PersonalInfoSampleApp/Pages/Form/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PersonalInfoSampleApp.Model;
+using PersonalInfoSampleApp.Persistence;
+
+namespace PersonalInfoSampleApp.Pages.Form
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DatabaseContext _context;
+
+        public IndexModel(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IList<PersonalInfo> PersonalInfo { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            PersonalInfo = await _context.PersonalInfo.ToListAsync();
+        }
+    }
+}
