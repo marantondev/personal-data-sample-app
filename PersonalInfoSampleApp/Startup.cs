238fe3c05cb897a086a5fbc157c289739ee7691c
Marlena Anton <marantondev@gmail.com>
2019-07-28 12:03:32 +0200

    Revert "Removed cookie policy"
    This reverts commit ba784a173eeea5c6c185aa0fc01b9aa38cdda0bc.

--- PersonalInfoSampleApp/Startup.cs
+++ PersonalInfoSampleApp/Startup.cs
@@ -26,6 +26,14 @@ namespace PersonalInfoSampleApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddDbContext<DatabaseContext>(options =>
@@ -47,6 +55,7 @@ namespace PersonalInfoSampleApp
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
 
             app.UseMvc();
         }
